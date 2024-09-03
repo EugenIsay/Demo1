6d432e9f5594396de086d916afacde37708c43bf
GafulOne <Zemchenkov23@gmail.com>
2024-09-03 16:21:40 +0300

    0.0

---
+++ Demo1/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Demo1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
